d687882a9b700a276b3d8fa9d5f4a2ab4d5a9456
MaiGao886 <1309473883@qq.com>
2021-02-04 16:59:35 +0800

    修改用户表

--- src/main/java/com/maigao/gogogo/entity/po/User.java
+++ src/main/java/com/maigao/gogogo/entity/po/User.java
@@ -5,15 +5,25 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Date;
 
 @Entity
 @Data
 public class User implements Serializable {
 
     @Id
-    private String id;//主键ID，使用字符串类型（30）
-    private String name;//名字，字符串（50）
+    private String id;//主键ID，使用字符串类型（64）
+    private String username;//用户名，字符串（64）
+    private String password;//密码，字符串（64）
+    private String nickname;//昵称，字符串（64）
     private Integer age;//年龄，integer
     private Boolean sex;//性别
+    private String mobile;//手机号，字符串（20）
+    private String email;//邮箱，字符串（64）
+    private Date createTime;//创建时间
+    private Date loginTime;//登陆时间
+    private Date lastLoginTime;//上次登陆时间
+    private BigInteger count;//登陆次数
 
 }
--- src/test/java/com/maigao/gogogo/Test.java
+++ src/test/java/com/maigao/gogogo/Test.java
@@ -1,25 +1,47 @@
 package com.maigao.gogogo;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
+        Test test = new Test();
+        long start = System.currentTimeMillis();
+        List<Integer> integers = test.addToArrayForm(new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9,9,9,9,9,9,9,9,9},34234234);
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+        System.out.println(Arrays.toString(integers.toArray()));
 
+    }
 
-        RandomAccessFile raf = null;
-        try {
-            raf = RAFTestFactory.getRAFWithModelRW();
-            raf.seek(2);
-            String word = "ljh";
-            raf.write(word.getBytes());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> res = new ArrayList<Integer>();
+        int n = A.length;
+        for (int i = n - 1; i >= 0; --i) {
+            int sum = A[i] + K % 10;
+            K /= 10;
+            if (sum >= 10) {
+                K++;
+                sum -= 10;
+            }
+            res.add(sum);
         }
+        for (; K > 0; K /= 10) {
+            res.add(K % 10);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = target - nums[i];
+            if (hashMap.containsKey(j)){
 
-
+                return new int[]{hashMap.get(j),i};
+            }
+            hashMap.put(nums[i],i);
+        }
+        throw new IllegalArgumentException();
     }
 }
